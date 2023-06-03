265532ad030402c1d1ece50d0e9640db3c9916c0
helioPompei <helioberizonzipompei2017@gmail.com>
2023-06-03 12:33:35 -0300

    Finishing the course

--- 14-Restfull_com_TypeScript/config/default.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  port: 5000,
-  dbURL: "mongodb://127.0.0.1:27017/database_test",
-  env: "development"
-};
--- 14-Restfull_com_TypeScript/src/router.ts
+++
@@ -1,8 +0,0 @@
-import { Router, Request, Response } from "express";
-const router = Router();
-
-router.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "Api working" });
-});
-
-export default router;
